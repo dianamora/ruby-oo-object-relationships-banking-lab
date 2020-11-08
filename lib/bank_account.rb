a0021f28491bca73d87260cf124e59f20943f428
Diana Mora <morra.dianna@gmail.com>
2020-11-08 11:33:32 -0500

    commit tree 6282e546a29a6e7d2a43c7a4c94d5b3a7b9fa034

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,33 @@
 class BankAccount
 
+  attr_accessor :balance, :status
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def deposit(deposit_amount) #can deposit money into its account #balance will say the same or be more
+    self.balance += deposit_amount
+  end
+
+  def display_balance
+    "Your balance is $#{balance}." 
+    #why don't we need 'self.'' here but need it for above method? is it b/c we are just 
+    #displaying the current balance so referring to instance varialbe instead of the class?
+  end 
+
+  def valid?
+    # valid = true if self.status == "open" && self.balance > 0 #why did this code not work?
+    balance > 0 && status == "open"
+  end
+
+  def close_account
+    self.status = "closed"
+  end
 end
+
+
+
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,58 @@
 class Transfer
-  # your code here
+  attr_reader :sender, :receiver, :amount
+  attr_accessor :status
+
+  def initialize(sender, receiver, amount)
+  @sender = sender
+  @receiver = receiver
+  @status = "pending"
+  @amount = amount
+  end
+
+  def valid?
+    sender.valid? && receiver.valid?
+  end
+
+ #rejects a transfer if the sender does not have enough funds (does not have a valid account)
+    def reject_transfer
+          self.status = "rejected"
+          "Transaction rejected. Please check your account balance."
+      end
+
+  def execute_transaction #can have sender send $ successfully to receiver #what needs to be in place in order for this to happen?
+    #both acounts have to be valid #sender has to have money to send
+    if valid? && sender.balance > amount && self.status == "pending"
+      sender.balance -= amount 
+      receiver.balance += amount
+      self.status = "complete"
+    else 
+      reject_transfer
+    end
+
+  end
+
+  def reverse_transfer
+    if valid? && receiver.balance > amount && self.status == "complete"
+      receiver.balance -= amount
+      sender.balance += amount
+      self.status = "reversed"
+    else
+      reject_transfer
+    end
+  end
+
 end
+
+
+
+
+#   def reverse_transfer
+#     if valid? && receiver.balance > amount && self.status == "complete"
+#       receiver.balance -= amount
+#       sender.balance += amount
+#       self.status = "reversed"
+#     else
+#       reject_transfer
+#     end
+#   end
+# end
